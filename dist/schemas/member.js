9ea923729d71607ac0700c36f8d70ccd27ea9cc7
Neal <Nedinator@users.noreply.github.com>
2024-09-09 13:20:36 -0500

    Delete dist directory manually

--- dist/schemas/member.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-const memberSchema = new Schema({
-    username: String,
-    userID: String,
-    warnings: { type: [{}], default: [] },
-    bans: { type: [{}], default: [] },
-});
-export const Member = model("Member", memberSchema);
--- dist/types/CustomClient.js
+++
@@ -1,7 +0,0 @@
-import { Client, Collection } from "discord.js";
-export class CustomClient extends Client {
-    constructor(options) {
-        super(options);
-        this.commands = new Collection();
-    }
-}
